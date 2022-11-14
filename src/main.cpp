4ae9adbe239388d5386882ea3829a07dbb0c6ae1
jane <jjt1525@outlook.com>
2022-11-14 17:33:40 +0800

    remove bar

--- src/main.cpp
+++ src/main.cpp
@@ -4,35 +4,30 @@
 #include "memory.h"
 #include "net.h"
 #include "version.h"
-#include "bar.h"
 
 #define INTERVAL 1
 
 void set_root(std::string);
 
 int main() {
-  std::string b;
+  std::string bar;
   bar::Version version;
   bar::Net net;
   bar::Memory memory;
   bar::CurTime curTime;
-  bar::Bar bar;
 
   while (1) {
-    b = "";
-    b += version.get();
-    b += net.get();
-    b += memory.get();
-    b += curTime.get();
+    bar = "xsetroot -name \"";
+    bar += version.get();
+    bar += net.get();
+    bar += memory.get();
+    bar += curTime.get();
+    bar += "\"";
 
-    bar.set_root(b);
+    system(bar.data());
     sleep(INTERVAL);
   }
 
   return 0;
-}
-
-void set_root(std::string bar) {
-  FILE *fp;
 }
 
